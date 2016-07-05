a0b04e1b25025ae8cf6244d016b4205f26873918
Jordan Singer <singer.jordan@gmail.com>
2016-07-04 19:39:34 -0700

    Hook 2.0: simplified

--- README.md
+++ README.md
@@ -1,38 +1,25 @@
-Hook.js
+Hook
 =======
 
-Pull to refresh. For the web. This rewrite is a work in progress and has only been tested on the latest Safari, Chrome, and Firefox. There are a few bugs that I'm currently working through with Firefox.
+Pull to refresh for the web
 
-##Dependency
-* Needs mousewheel.js, however will most likely change this to an option in future commits.
+##Dependencies
+* jQuery
 
-##There are 9 settings and 1 callback. Which are the following;
-* reloadPage: default is true, if false will reload element (note must also have reloadEl callback),
-* dynamic: default is true, if false you will need your own html,
-* textRequired: default is false, if true the dynamic text HTML will be added,
-* swipeDistance: default is 50, the amount of swipe distance on touch devices to fire reload,
-* loaderClass: default is hook-loader,
-* spinnerClass: default is hook-spinner,
-* loaderTextClass: default is hook-text,
-* loaderText: default is 'Reloading..' is only set when you set 'textRequired'
-* reloadEl: callback
-
-##Outstanding issues:
-* Firefox reload only appearing after first scrolling down.
-
-##ToDo's: 
-* Convert from jQuery animate to css3 transitions for browsers that support it.
+##Settings
+* refresh: refreshes entire page, default is true, if false will call callback (must also have callback setting),
+* callback: custom callback function
 
 ##Examples:
 ```` JS
-// No options
+// Defaults
 $('#hook').hook();
 
 // Callback
 $('#hook').hook({
-  reloadPage: false,
-  reloadEl: function(){
-    console.log('Hello World!');
+  refresh: false,
+  callback: function(){
+    console.log('Hello, World!');
   }
 });
 ````
--- hook.js
+++ hook.js
@@ -1,187 +1,57 @@
 /**
  * Hook
- * Version: 1.1
+ * Version: 2.0
  * Author: Jordan Singer, Brandon Jacoby, Adam Girton
- * Copyright (c) 2013 - Hook.  All rights reserved.
- * http://www.usehook.com
+ * Copyright (c) 2016 Hook. All rights reserved.
+ * https://github.com/jordansinger/Hook
  */
-
-;(function ( $, window, document, undefined ) {
+(function($, window, document, undefined) {
     var win = $(this),
-            st = win.scrollTop() || window.pageYOffset,
-            called = false;
+        st = win.scrollTop() || window.pageYOffset,
+        called = false;
 
-    var hasTouch = function() {
-              return !!('ontouchstart' in window) || !!('onmsgesturechange' in window);
-            };
-
-      var handlers = {};
-
-      var addHandler = function(name, fn) {
-        win.on(name, fn);
-        handlers[name] = fn;
-      };
-      var removeHandler = function(name, fn) {
-        win.off(name, handlers[name]);
-        delete handlers[name];
-      };
-      var removeHandlers = function() {
-        for (var name in handlers) {
-          removeHandler(name);
-        }
-      };
-
-      var methods = {
-
+    var methods = {
         init: function(options) {
-
-                return this.each(function() {
-                    var $this = $(this),
-                        settings = $this.data('hook');
-
-                        if(typeof(settings) === 'undefined') {
-
-                                var defaults = {
-                                    reloadPage: true, // if false will reload element
-                                    dynamic: true, // if false Hook elements already there
-                                    textRequired: false, // will input loader text if true
-                                    scrollWheelSelected: false, // will use scroll wheel events
-                                    swipeDistance: 50, // swipe distance for loader to show on touch devices
-                                    loaderClass: 'hook-loader',
-                                    spinnerClass: 'hook-spinner',
-                                    loaderTextClass: 'hook-text',
-                                    loaderText: 'Reloading...',
-                                    reloadEl: function() {}
-                                };
-
-                                settings = $.extend({},  defaults, options);
-
-                                $this.data('hook', settings);
-                        } else {
-
-                                settings = $.extend({}, settings, options);
-                        }
-
-                        if(settings.dynamic === true) {
-                             var loaderElem = '<div class=' + settings.loaderClass + '>';
-                                     loaderElem += '<div class='+ settings.spinnerClass + '/>';
-                                     loaderElem += '</div>';
-                             var spinnerTextElem = '<span class='+ settings.loaderTextClass + '>' + settings.loaderText + '</span>';
-
-                             $this
-                                     .append(loaderElem);
-
-                             if (settings.textRequired === true) {
-                                  $this.addClass('hook-with-text');
-                                  $this.append(spinnerTextElem);
-                             }
-                        }
-
-                        if(!hasTouch()) {
-                            if(settings.scrollWheelSelected === true){
-                              addHandler('mousewheel', function(event, delta) {
-                                  methods.onScroll($this, settings, delta);
-                              })
-                            } else {
-                              addHandler('scroll', function() {
-                                  methods.onScroll($this, settings);
-                              });
-                            }
-                        }  else {
-                            var lastY = 0,
-                                 swipe = 0;
-                            addHandler('touchstart', function(e){
-                                lastY = e.originalEvent.touches[0].pageY;
-                            });
-
-                            addHandler('touchmove', function(e) {
-                                swipe = e.originalEvent.touches[0].pageY + lastY;
-                                st = $(this).scrollTop();
-
-                                if(swipe < settings.swipeDistance) {
-                                  e.preventDefault();
-                                }
-
-                                if(swipe > settings.swipeDistance && lastY <= 40) {
-                                    methods.onSwipe($this, settings);
-                                }
-                            });
-
-                            addHandler('touchend', function(){
-                                swipe = 0;
-                            });
-                        }
-
-                });
-        },
-
-        onScroll: function(el, settings, delta) {
-          st = win.scrollTop();
-
-          if(settings.scrollWheelSelected === true && (delta >= 150 && st <= 0)) {
-              if(called === false) {
-                  methods.reload(el, settings);
-                  called = true;
-              }
-          }
-
-          if(settings.scrollWheelSelected === false && st <= 0) {
-            if(called === false) {
-                methods.reload(el, settings);
-                called = true;
-            }
-          }
-        },
-
-        onSwipe: function(el, settings) {
-            if(st <= 0) {
-                methods.reload(el, settings);
-            }
-        },
-
-        reload: function(el, settings) {
-                el.show();
-                el.animate({
-                    "marginTop": "0px"
-                }, 200);
-                el.delay(500).slideUp(200, function () {
-                    if(settings.reloadPage) {
-                        window.location.reload(true);
-                    }
-
-                    called = false;
-                });
-
-                if(!settings.reloadPage) {
-                    settings.reloadEl();
+            return this.each(function() {
+                var $this = $(this),
+                    settings = $this.data("hook");
+                if (typeof(settings) === "undefined") {
+                    var defaults = {
+                        refresh: true, // refresh the page
+                        callback: function() {}
+                    };
+                    settings = $.extend({}, defaults, options);
+                    $this.data("hook", settings);
+                } else {
+                    settings = $.extend({}, settings, options);
                 }
-        },
 
-        destroy: function() {
-            removeHandlers();
-            return $(this).each(function(){
-                var $this = $(this);
-
-                $this.empty();
-                $this.removeData('hook');
+                $(window).scroll(function(event) {
+                    var st = $(window).scrollTop();
+                    if (st <= 0) {
+                        if (!settings.refresh) {
+                            settings.callback();
+                        } else {
+                            $this.addClass("fadeIn");
+                            window.location.reload();
+                        }
+                    }
+                });
             });
         }
     };
 
-    $.fn.hook = function () {
+    $.fn.hook = function() {
         var method = arguments[0];
-
-        if(methods[method]) {
+        if (methods[method]) {
             method = methods[method];
             arguments = Array.prototype.slice.call(arguments, 1);
-        } else if (typeof(method) === 'object' || !method) {
+        } else if (typeof(method) === "object" || !method) {
             method = methods.init;
         } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.pluginName' );
-                        return this;
+            $.error("Method " + method + " does not exist on jQuery.pluginName");
+            return this;
         }
-
         return method.apply(this, arguments);
     };
-
-})( jQuery, window, document );
+})(jQuery, window, document);
